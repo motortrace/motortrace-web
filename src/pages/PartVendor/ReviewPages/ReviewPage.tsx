723142d40fab83b32fbd018487d620c9c9875c8a
SadheeyaM <sadheeyasalim10@gmail.com>
2025-07-09 19:59:52 +0530

    Added Review Page

---
+++ src/pages/PartVendor/ReviewPages/ReviewPage.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import ReviewsCard from '../../../components/PartVendorComponents/ReviewPageComponents/ReviewsCard/ReviewsCard'
+import DetailedReviews from '../../../components/PartVendorComponents/ReviewPageComponents/DetailedReviews/DetailedReviews'
+
+const ReviewPage = () => {
+  return (
+    <div>
+      <ReviewsCard/>
+      <DetailedReviews/>
+    </div>
+  )
+}
+
+export default ReviewPage
