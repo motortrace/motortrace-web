58fa4473225c2c203d648a51f8882945efef2971
SadheeyaM <sadheeyasalim10@gmail.com>
2025-07-17 13:21:38 +0530

    Added new components to the reviews page

--- src/pages/PartVendor/ReviewPages/ReviewPage.tsx
+++ src/pages/PartVendor/ReviewPages/ReviewPage.tsx
@@ -1,14 +1,35 @@
-import React from 'react'
-import ReviewsCard from '../../../components/PartVendorComponents/ReviewPageComponents/ReviewsCard/ReviewsCard'
-import DetailedReviews from '../../../components/PartVendorComponents/ReviewPageComponents/DetailedReviews/DetailedReviews'
+import React from 'react';
+import './ReviewPage.scss';
+import UnrepliedReviewsCard from '../../../components/PartVendorComponents/ReviewPageComponentsNew/UnrepliedReviewsCard/UnrepliedReviewsCard';
+import RatingsOverviewCard from '../../../components/PartVendorComponents/ReviewPageComponentsNew/RatingOverviewCard/RatingOverviewCard';
+import RatingComparisonChart from '../../../components/PartVendorComponents/ReviewPageComponentsNew/RatingBreakdownCard/RatingBreakdownCard';
+import ReviewTrendsChart from '../../../components/PartVendorComponents/ReviewPageComponentsNew/ReviewTrendsChart/ReviewTrendsChart';
+import DetailedReviewsSection from '../../../components/PartVendorComponents/ReviewPageComponentsNew/DetailedReview/DetailedReviewSection';
+
 
 const ReviewPage = () => {
   return (
     <div>
-      <ReviewsCard/>
-      <DetailedReviews/>
-    </div>
-  )
-}
+      <div className="reviews-page">
+        <div className="ratings-overview">
+          <RatingsOverviewCard />
+        </div>
+        <div className="rating-stats">
+          <RatingComparisonChart />
+        </div>
+        <div className="unreplied-reviews">
+          <UnrepliedReviewsCard />
+        </div>
+        <div className="review-trends">
+          <ReviewTrendsChart />
+        </div>
+      </div>
 
-export default ReviewPage
+      <div className="reviews-detailed-section">
+        <DetailedReviewsSection/>
+      </div>
+    </div>
+  );
+};
+
+export default ReviewPage;
