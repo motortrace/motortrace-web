8cdb01257d6b0ec1bf3d7a87aad49f21c5cfd623
SadheeyaM <sadheeyasalim10@gmail.com>
2025-07-19 17:01:44 +0530

    Added navigation to customer details page from the order details page

--- src/components/PartVendorComponents/OrderSummaryPageComponents/OrderDetailsSidebar/OrderDetailsSidebar.tsx
+++ src/components/PartVendorComponents/OrderSummaryPageComponents/OrderDetailsSidebar/OrderDetailsSidebar.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import './OrderDetailsSidebar.scss';
+import { useNavigate } from 'react-router-dom';
+import { ArrowUpRight } from 'lucide-react';
 
 interface CustomerDetails {
   name: string;
@@ -31,20 +33,34 @@ const OrderDetailsSidebar: React.FC<OrderDetailsSidebarProps> = ({
   billing,
   shipping,
 }) => {
+  const navigate = useNavigate();
+
+  const handleCustomerNavigate = () => {
+    // const encodedName = encodeURIComponent(customer.name);
+    navigate(`/PartVendor/CustomerDetails`);
+  };
+
   return (
     <div className="order-details-sidebar">
       <div className="order-details-sidebar__section">
-        <h4 className="order-details-sidebar__section-title">Customer Details</h4>
+        <div className="order-details-sidebar__section-header">
+          <h4 className="order-details-sidebar__section-title">Customer Details</h4>
+          <button
+            className="order-details-sidebar__arrow-button"
+            onClick={handleCustomerNavigate}
+            title={`Go to ${customer.name}'s profile`}
+          >
+            <ArrowUpRight size={14} />
+          </button>
+        </div>
+
         <div className="order-details-sidebar__info">
-          {/* <span>Name:</span> */}
           <span>{customer.name}</span>
         </div>
         <div className="order-details-sidebar__info">
-          {/* <span>Email:</span> */}
           <span>{customer.email}</span>
         </div>
         <div className="order-details-sidebar__info">
-          {/* <span>Phone:</span> */}
           <span>{customer.phone}</span>
         </div>
       </div>
@@ -54,11 +70,9 @@ const OrderDetailsSidebar: React.FC<OrderDetailsSidebarProps> = ({
       <div className="order-details-sidebar__section">
         <h4 className="order-details-sidebar__section-title">Payment Details</h4>
         <div className="order-details-sidebar__info">
-          {/* <span>Method:</span> */}
           <span>{payment.method}</span>
         </div>
         <div className="order-details-sidebar__info">
-          {/* <span>Status:</span> */}
           <span>{payment.status}</span>
         </div>
       </div>
@@ -68,16 +82,13 @@ const OrderDetailsSidebar: React.FC<OrderDetailsSidebarProps> = ({
       <div className="order-details-sidebar__section">
         <h4 className="order-details-sidebar__section-title">Billing Details</h4>
         <div className="order-details-sidebar__info">
-          {/* <span>Address:</span> */}
           <span>{billing.addressLine1}</span>
         </div>
         <div className="order-details-sidebar__info">
-          {/* <span>City:</span> */}
           <span>{billing.city}</span>
         </div>
         {billing.country && (
           <div className="order-details-sidebar__info">
-            {/* <span>Country:</span> */}
             <span>{billing.country}</span>
           </div>
         )}
@@ -88,16 +99,13 @@ const OrderDetailsSidebar: React.FC<OrderDetailsSidebarProps> = ({
       <div className="order-details-sidebar__section">
         <h4 className="order-details-sidebar__section-title">Shipping Details</h4>
         <div className="order-details-sidebar__info">
-          {/* <span>Address:</span> */}
           <span>{shipping.addressLine1}</span>
         </div>
         <div className="order-details-sidebar__info">
-          {/* <span>City:</span> */}
           <span>{shipping.city}</span>
         </div>
         {shipping.country && (
           <div className="order-details-sidebar__info">
-            {/* <span>Country:</span> */}
             <span>{shipping.country}</span>
           </div>
         )}
