dda21be181ffab12f295a31e60278310aee688b9
jabir0331 <mnnjabir@gmail.com>
2025-09-09 11:53:28 +0530

    Enahnced the login interface view and integrated  with backend

--- src/utils/fetchUserStatus.ts
+++ src/utils/fetchUserStatus.ts
@@ -2,7 +2,7 @@ export async function fetchUserStatus() {
   const token = localStorage.getItem('token');
   if (!token) return null;
   try {
-    const res = await fetch('http://localhost:3000/user/status', {
+    const res = await fetch(`${import.meta.env.VITE_BACKEND_URL}/auth/me`, {
       headers: { Authorization: `Bearer ${token}` }
     });
     if (!res.ok) return null;
